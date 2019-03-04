61737a496688f02f1c4e79bf46856aa96db9eab7
muhwezi deo <aggrey256@gmail.com>
2019-03-04 20:56:20 +0300

    constants

---
+++ constants.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dee
+ * Date: 2019-03-04
+ * Time: 20:54
+ */
+define('NEW_CONSTANT','a constant');
+//constants are global variables
+echo NEW_CONSTANT;
