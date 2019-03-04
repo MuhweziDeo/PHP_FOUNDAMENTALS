f3d47178e2b2b59e24f6ca5992118059d1980d0d
muhwezi deo <aggrey256@gmail.com>
2019-03-04 20:54:03 +0300

    boolean

---
+++ boolean.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dee
+ * Date: 2019-03-04
+ * Time: 20:51
+ */
+$bool = true;
+
+$hasVale=1235;
+//convert value to boolean
+var_dump((bool) $hasVale);
+
+var_dump($bool);
