86309722ae72d9757248145b74be7df888966a05
muhwezi deo <aggrey256@gmail.com>
2019-03-04 21:16:20 +0300

    basic functions

---
+++ Functions/createFunction.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dee
+ * Date: 2019-03-04
+ * Time: 21:09
+ */
+//basic function
+function bookAuthor (){
+    echo 'deee';
+    echo 'agggr';
+};
+
+bookAuthor();
+// function params
+function bookByAuthor($authorName){
+    echo $authorName;
+}
+
+bookByAuthor('Deee');
