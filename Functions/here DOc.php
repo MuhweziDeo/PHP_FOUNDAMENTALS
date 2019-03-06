0a441d890e6c97f56e84f1b501494d278fce9d7e
muhwezi deo <aggrey256@gmail.com>
2019-03-06 20:03:53 +0300

    here doc

---
+++ Functions/here DOc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dee
+ * Date: 2019-03-06
+ * Time: 20:03
+ */
+echo <<<EOT
+bhe dlsdasmndasdlmsa
+sfsalmasflfa
+aslmalfalsmsl
+saflsmflm
+EOT;
---
+++ Functions/variableFunctions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dee
+ * Date: 2019-03-06
+ * Time: 19:37
+ */
+function getAuthor(){
+    echo "Muhwezi Deo";
+};
+
+$variableFunctionName= 'getAuthor';
+$variableFunctionName();
